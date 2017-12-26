bd5b8287bdd984da15b9a15425b5d67fce7b1581
Mark Wolff <mark.wolff@pooliestudios.com>
2017-12-26 17:44:05 +0100

    Symfony add some fancy twig

--- api/src/Controller/DefaultController.php
+++ api/src/Controller/DefaultController.php
@@ -3,13 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController
 {
 
     public function index ()
     {
-        return new Response('<html><head></head><body><h1>Hello World.</h1></body></html>');
+        return $this->render('base.html.twig');
     }
 }
