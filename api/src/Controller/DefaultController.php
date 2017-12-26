71362e5adc84e14633e79173f6fbf935e4dd5f22
Mark Wolff <mark.wolff@pooliestudios.com>
2017-12-26 18:12:17 +0100

    Symfony add apache-pack / symfony annotations

--- api/src/Controller/DefaultController.php
+++ api/src/Controller/DefaultController.php
@@ -3,10 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends AbstractController
 {
 
+    /**
+     * @Route("/test-mark", name="mark")
+     */
     public function index ()
     {
         return $this->render('base.html.twig');
